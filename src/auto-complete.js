bacd0e8111b3f0bfd3825e0c8f8ced44ae00593c
nicolascepeda <cepeda.nicolas@gmail.com>
2015-04-14 17:29:19 +0200

    Fixed Bug cannot click on firefox autocomplete

--- src/auto-complete.js
+++ src/auto-complete.js
@@ -183,7 +183,9 @@ tagsInput.directive('autoComplete', function($document, $timeout, $sce, $q, tags
 
             tagsInput
                 .on('tag-added tag-removed invalid-tag input-blur', function() {
-                    suggestionList.reset();
+                    $timeout(function(){
+                        suggestionList.reset();
+                    }, 200);
                 })
                 .on('input-change', function(value) {
                     if (shouldLoadSuggestions(value)) {
